e933cc2990bfe6d750e6615482bf88c46c2bb864
Blisstod <113333375+Blisstod@users.noreply.github.com>
2023-02-07 13:28:15 +0600

    Update EventController.java

--- OOP_Assignment3/src/com/company/controllers/EventController.java
+++ OOP_Assignment3/src/com/company/controllers/EventController.java
@@ -1,4 +1,24 @@
 package com.company.controllers;
 
+import com.company.entities.Event;
+import com.company.repositories.EventRepositories;
+import com.company.repositories.interfaces.IEventRepositories;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventController {
+    private final IEventRepositories eventRepositories;
+    public EventController(IEventRepositories eventRepositories){
+        this.eventRepositories = eventRepositories;
+    }
+    public String CreateEvent(String name, double price, String description){
+        Event event = new Event(name, price, description);
+        boolean created = eventRepositories.CreateEvent(event);
+        return (created ? "Event was created" : "Event creation was failed!");
+    }
+    public String getAllEvents(){
+        List<Event> events = eventRepositories.getAllEvents();
+        return events.toString();
+    }
 }
