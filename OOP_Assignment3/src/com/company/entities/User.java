c1637199ce4048460078b00dd40a7e0bcd84b9a1
Blisstod <113333375+Blisstod@users.noreply.github.com>
2023-02-07 13:28:49 +0600

    Update User.java

--- OOP_Assignment3/src/com/company/entities/User.java
+++ OOP_Assignment3/src/com/company/entities/User.java
@@ -1,29 +1,16 @@
 package com.company.entities;
 
 public class User {
-    private int id;
     private String name;
     private String surname;
-    private boolean gender;
     private double balance;
-    public User(){}
-    public User(String name, String surname, boolean gender, double balance) {
-        setName(name);
-        setSurname(surname);
-        setGender(gender);
+    public User(double balance){
         setBalance(balance);
     }
-    public User(int id, String name, String surname, boolean gender, double balance) {
-        this(name, surname, gender, balance);
-        setId(id);
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
+    public User(String name, String surname, double balance) {
+        setName(name);
+        setSurname(surname);
+        setBalance(balance);
     }
 
     public void setName(String name) {
@@ -42,12 +29,6 @@ public class User {
         return surname;
     }
 
-    public void setGender(boolean gender) {
-        this.gender = gender;
-    }
-    public boolean getGender() {
-        return gender;
-    }
 
     public void setBalance(double balance) {
         this.balance = balance;
@@ -57,14 +38,11 @@ public class User {
         return balance;
     }
 
-
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
                 ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
-                ", gender=" + gender + '\'' +
                 ", balance=" + balance +
                 '}';
     }
