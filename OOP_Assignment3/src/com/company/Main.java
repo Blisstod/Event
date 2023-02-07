3cdd2b9b6e8589ce879349410270d1bac19551aa
Blisstod <113333375+Blisstod@users.noreply.github.com>
2023-02-07 13:32:08 +0600

    Update Main.java

--- OOP_Assignment3/src/com/company/Main.java
+++ OOP_Assignment3/src/com/company/Main.java
@@ -1,24 +1,18 @@
 package com.company;
 
 import com.company.controllers.EventController;
-import com.company.controllers.UserController;
 import com.company.data.PostgresDB;
 import com.company.data.interfaces.IDB;
 import com.company.repositories.EventRepositories;
-import com.company.repositories.UserRepository;
 import com.company.repositories.interfaces.IEventRepositories;
-import com.company.repositories.interfaces.IUserRepository;
 
-import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
         IDB db = new PostgresDB();
         IEventRepositories eventRepositories = new EventRepositories(db);
-        IUserRepository userRepository = new UserRepository(db);
-        EventController eventController = new EventController(eventRepositories)
-        UserController userController = new UserController(userRepository);
-        EventApplication app = new EventApplication(controller);
+        EventController eventController = new EventController(eventRepositories);
+        EventApplication app = new EventApplication(eventController);
         app.start();
     }
 }
