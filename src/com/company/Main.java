4b29bdb5ee4909507e4ad8e1c021ce3bc16301e7
arsenserikkaliyev <arsen.serikkaliev.04@mail.ru>
2023-02-14 09:08:21 +0600

    h

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import com.company.controllers.EventController;
+
+import com.company.controllers.RegisterEventController;
+import com.company.data.PostgresDB;
+import com.company.data.interfaces.IDB;
+import com.company.repositories.EventRepositories;
+import com.company.repositories.interfaces.IEventRepositories;
+
+
+public class Main {
+    public static void main(String[] args) {
+        IDB db = new PostgresDB();
+        IEventRepositories eventRepositories = new EventRepositories(db);
+        EventController eventController = new EventController(eventRepositories);
+        RegisterEventController registerEventController = new RegisterEventController();
+        EventApplication app = new EventApplication(eventController,registerEventController);
+        app.start();
+    }
+}
