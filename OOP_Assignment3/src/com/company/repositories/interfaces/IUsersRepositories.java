17d71ba4f682d946a0b3cf2562e002f3d06f44a5
Blisstod <113333375+Blisstod@users.noreply.github.com>
2023-02-15 18:42:48 +0600

    Update IUsersRepositories.java

--- OOP_Assignment3/src/com/company/repositories/interfaces/IUsersRepositories.java
+++ OOP_Assignment3/src/com/company/repositories/interfaces/IUsersRepositories.java
@@ -7,7 +7,6 @@ import java.util.List;
 public interface IUsersRepositories {
     boolean CreateUser(User user);
     User SignIn(User userToCheck);
-    boolean isEmpty ();
     boolean isExist(User user);
     List<User> getAllUsers();
 }
